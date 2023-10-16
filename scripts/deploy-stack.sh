dd53759c3a97109da65b59f9a14f680b1d407023
ryan-talus <rzimmerman@talusanalytics.com>
2023-10-16 12:26:53 -0300

    adding comment

--- scripts/deploy-stack.sh
+++ scripts/deploy-stack.sh
@@ -1,3 +1,8 @@
+# Command used to deploy or update the database stack
+
+# --rofile needs to be an AWS Profile in the Verena
+# account with  developer-level permissions
+
 sam deploy \
   --config-env prod \
   --template-file ./template.yaml \
